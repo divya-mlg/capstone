0f7419c540796b71545a18b8f6b412a360dfc1d0
divya <divyavandanapu112@gmail.com>
2023-02-12 12:15:28 -0800

    redis changes

--- backend/app_v1.py
+++ backend/app_v1.py
@@ -1,26 +1,34 @@
-from flask import Flask
+from flask import Flask, abort
 from flask import request
+import redis
+import os
 
 app = Flask(__name__)
 
-counter_val = 1
+# connect to a redis instance and initialize index_counter
+red = redis.Redis.from_url(os.environ.get('REDIS_FROM_URL'))
+print(red.ping())
+red.set("index_counter", 1)
 
 def get_counter():
-    return str(counter_val)
+    counter_val = red.get("index_counter")
+    return counter_val.decode('utf-8')
+
 
 def increment_counter():
-    global counter_val
+    counter_val = int(red.get("index_counter"))
     counter_val = counter_val + 1
+    red.set("index_counter", counter_val)
     return str(counter_val)
     
 @app.route('/api/counter', methods=["GET", "POST"])
 def counter():
     if request.method == "GET":
         return get_counter()
-    else:
+    elif request.method == "POST":
         return increment_counter()
 
-    return counter_val
+    abort(405)
 
 if __name__ == '__main__':
     app.run()
--- backend/app_v2.py
+++ backend/app_v2.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, abort
 from flask import request
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+import redis
+import os
 
 trace.set_tracer_provider(
 TracerProvider(
@@ -33,24 +35,30 @@ trace.get_tracer_provider().add_span_processor(span_processor)
 
 app = Flask(__name__)
 
-counter_val = 1
+# connect to a redis instance and initialize index_counter
+red = redis.Redis.from_url(os.environ.get('REDIS_FROM_URL'))
+print(red.ping())
+red.set("index_counter", 1)
 
 def get_counter():
-    return str(counter_val)
+    counter_val = red.get("index_counter")
+    return counter_val.decode('utf-8')
+
 
 def increment_counter():
-    global counter_val
+    counter_val = int(red.get("index_counter"))
     counter_val = counter_val + 1
+    red.set("index_counter", counter_val)
     return str(counter_val)
     
 @app.route('/api/counter', methods=["GET", "POST"])
 def counter():
     if request.method == "GET":
         return get_counter()
-    else:
+    elif request.method == "POST":
         return increment_counter()
 
-    return counter_val
+    abort(405)
 
 if __name__ == '__main__':
     app.run()
