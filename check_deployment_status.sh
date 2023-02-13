ef3fce3e8ea0f53ac99fec535f4654da03aa40db
divya <divyavandanapu112@gmail.com>
2023-02-13 08:16:37 -0800

    Updated to load balancer service type

--- check_deployment_status.sh
+++ check_deployment_status.sh
@@ -3,7 +3,7 @@ dep_name=$1
 echo "Current deployment status of $dep_name before entering wait loop:"
 kubectl get deployments/$dep-name -o wide
 for i in {1..10}; do
-    kubectl rollout status deployment/$dep_name --timeout=1s
+    kubectl rollout status deployment/$dep_name --timeout=2s
     if [[ "$?" -eq 0 ]]; then
         kubectl get deployments/$dep_name -o wide
         exit 0
