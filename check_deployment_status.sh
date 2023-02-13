75dd2a321be2f6bded08899f1a9b3e70cd72277c
divya <divyavandanapu112@gmail.com>
2023-02-12 17:00:04 -0800

    jobs with updated deployments

--- check_deployment_status.sh
+++ check_deployment_status.sh
@@ -1,9 +1,11 @@
 
 dep_name=$1
+echo "Current deployment status of $dep_name before entering wait loop:"
+kubectl get deployments/$dep-name -o wide
 for i in {1..10}; do
     kubectl rollout status deployment/$dep_name --timeout=1s
     if [[ "$?" -eq 0 ]]; then
-        kubectl get deployments/$dep_name
+        kubectl get deployments/$dep_name -o wide
         exit 0
     fi
 done
